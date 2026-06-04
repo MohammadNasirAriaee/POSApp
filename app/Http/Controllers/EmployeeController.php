c6e9ab906f38f3efb77746321d65358607b227ec
MohammadNasirAriaee <nasirariaee@gmail.com>
2026-06-04 10:24:26 +0430

    my update

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -30,6 +30,7 @@ class EmployeeController extends Controller
     }
 
     /**
+     * 
      * Store a newly created resource in storage.
      */
     public function store(StoreEmployeeRequest $request)
