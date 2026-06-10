c5722f65f8218d13476368efd2d6909c7f57728a
MohammadNasirAriaee <nasirariaee@gmail.com>
2026-06-10 07:48:46 +0430

    asdf

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -49,10 +49,8 @@ class EmployeeController extends Controller
      * Show the form for editing the specified resource.
      */
     public function edit(Employee $employee)
-
     {
         //
-        
     }
 
     /**
