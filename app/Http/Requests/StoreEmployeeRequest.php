c7349008a5c33499759e4826d814455c61343a1c
MohammadNasirAriaee <nasirariaee@gmail.com>
2026-06-16 14:32:28 +0430

    asdf

--- app/Http/Requests/StoreEmployeeRequest.php
+++ app/Http/Requests/StoreEmployeeRequest.php
@@ -28,6 +28,7 @@ class StoreEmployeeRequest extends FormRequest
 			'phone' => ['nullable', 'string', 'max:20'],
 			'address' => ['nullable', 'string', 'max:500'],
 			'position' => ['required', 'string', 'max:100'],
+            
 			'salary' => ['nullable', 'numeric', 'min:0'],
 			'hire_date' => ['nullable', 'date'],
 			'status' => ['nullable', 'in:active,inactive'],
