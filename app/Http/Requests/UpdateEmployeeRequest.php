45064409727a7e543a243790df2bb0878ef86e6e
MohammadNasirAriaee <nasirariaee@gmail.com>
2026-06-18 09:15:57 +0430

    asdf

--- app/Http/Requests/UpdateEmployeeRequest.php
+++ app/Http/Requests/UpdateEmployeeRequest.php
@@ -29,8 +29,9 @@ class UpdateEmployeeRequest extends FormRequest
 			'address' => ['nullable', 'string', 'max:500'],
 
 			'position' => ['required', 'string', 'max:100'],
-			'salary' => ['nullable', 'numeric', 'min:0'],
             
+			'salary' => ['nullable', 'numeric', 'min:0'],
+
 			'status' => ['nullable', Rule::in(['active', 'inactive'])],
 		];
 	}
