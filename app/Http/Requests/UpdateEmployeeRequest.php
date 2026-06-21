be0a8bfc5d0762466fd6fefdf107e457a4df2a91
MohammadNasirAriaee <nasirariaee@gmail.com>
2026-06-21 11:01:54 +0430

    asdf

--- app/Http/Requests/UpdateEmployeeRequest.php
+++ app/Http/Requests/UpdateEmployeeRequest.php
@@ -17,6 +17,7 @@ class UpdateEmployeeRequest extends FormRequest
 		$employeeId = $this->route('employee')?->id ?? $this->route('employee');
 
 
+
         
 		return [
 			'first_name' => ['required', 'string', 'max:100'],
