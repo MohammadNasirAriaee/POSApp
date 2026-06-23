c900b11d577b3e0830196abd390609d9edebd1b2
MohammadNasirAriaee <nasirariaee@gmail.com>
2026-06-23 11:41:15 +0430

    asdf

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -26,7 +26,9 @@
                                 @method('DELETE')
                                 <button type="submit" class="btn btn-danger">Delete</button>
                             </form>
+
                         </td>
+                        
                     </tr>
                 @endforeach
             </tbody>
