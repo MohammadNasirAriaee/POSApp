1db30105594cc455f49dec0dad1dd516fbc4986a
MohammadNasirAriaee <nasirariaee@gmail.com>
2026-07-11 15:31:15 +0430

    asdf

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -36,6 +36,7 @@
                                             </td>
                                         </tr>
                                     @endforeach
+                                    
                                 </tbody>
                             </table>
                         </div>
