43dd8ec477eca0cf51078ab899ec0979176b572e
MohammadNasirAriaee <nasirariaee@gmail.com>
2026-07-21 13:41:21 +0430

    asdf

--- app/Http/Requests/UpdateEmployeeRequest.php
+++ app/Http/Requests/UpdateEmployeeRequest.php
@@ -23,7 +23,6 @@ class UpdateEmployeeRequest extends FormRequest
 				'max:255',
 				Rule::unique('employees', 'email')->ignore($employeeId),
 			],
-
 			'phone' => ['nullable', 'string', 'max:30'],
 			'address' => ['nullable', 'string', 'max:500'],
 			'position' => ['required', 'string', 'max:100'],
