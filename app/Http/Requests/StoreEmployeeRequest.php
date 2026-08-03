45f2fdef81fa06a888d566f0736ce4aee485f892
MohammadNasirAriaee <nasirariaee@gmail.com>
2026-08-03 14:52:18 +0430

    asdf

--- app/Http/Requests/StoreEmployeeRequest.php
+++ app/Http/Requests/StoreEmployeeRequest.php
@@ -21,6 +21,7 @@ class StoreEmployeeRequest extends FormRequest
 	 */
 	public function rules(): array
 	{
+        
 		return [
 			'first_name' => ['required', 'string', 'max:100'],
 			'last_name' => ['required', 'string', 'max:100'],
