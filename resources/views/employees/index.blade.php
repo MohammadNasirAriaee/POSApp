c17b6d9e63f76beba64f6795a24083b4fd85923b
MohammadNasirAriaee <nasirariaee@gmail.com>
2026-08-03 14:51:30 +0430

    asdf

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -26,6 +26,7 @@
                             <th class="px-4 py-3">Actions</th>
                         </tr>
                     </thead>
+                    
                     <tbody class="divide-y divide-slate-200 bg-white">
                         @foreach ($employees as $employee)
                             <tr>
