5b05c9cc1697924e9fd37527306540c500c3881e
MohammadNasirAriaee <nasirariaee@gmail.com>
2026-08-04 15:05:33 +0430

    asdfasdfdljasdf

--- app/Http/Requests/UpdateEmployeeRequest.php
+++ app/Http/Requests/UpdateEmployeeRequest.php
@@ -30,5 +30,6 @@ class UpdateEmployeeRequest extends FormRequest
 			'hire_date' => ['nullable', 'date', 'before_or_equal:today'],
 			'status' => ['nullable', Rule::in(['active', 'inactive', 'on_leave'])],
 		];
+        
 	}
 }
