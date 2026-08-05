7669ce0a66f2c034c2fee99e2eaf7f64aad6e80e
MohammadNasirAriaee <nasirariaee@gmail.com>
2026-08-05 10:47:22 +0430

    index changessecond timea

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -18,5 +18,5 @@
         </form>
     </div>
 </div>
-// end of section in this file!!! - meaningfull - if not meaning full at 
+// end of section in this file!!! - meaningfull - if not meaning full
 @endsection
