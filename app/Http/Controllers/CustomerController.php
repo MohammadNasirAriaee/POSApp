7ada0f93b45318c30af995588c5ada81cce836cc
MohammadNasirAriaee <nasirariaee@gmail.com>
2026-08-06 10:43:32 +0430

    asdfasdf

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -23,7 +23,7 @@ class CustomerController extends Controller
             });
         }
 
-        $customers = $query->paginate(12)->withQueryString(); // Paginate results with 12 items maintain query string parameters
+        $customers = $query->paginate(12)->withQueryString(); // Paginate results with 12 items query string parameters
 
         return Inertia::render('Customers/Index', compact('customers', 'search'));
     }
