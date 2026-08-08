ee1a32237179d3edf7e1620c7f088184b3023551
MohammadNasirAriaee <nasirariaee@gmail.com>
2026-08-08 15:28:49 +0430

    asdf

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -23,7 +23,7 @@ class CustomerController extends Controller
             });
         }
 
-        $customers = $query->paginate(12)->withQueryString(); // Paginatation of the customers list, 12 per page, and maintain the search query in the pagination links.
+        $customers = $query->paginate(12)->withQueryString(); // Paginatation of
 
         return Inertia::render('Customers/Index', compact('customers', 'search'));
     }
