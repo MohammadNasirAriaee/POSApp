bf3bebb42b79efaaac41838c4536627472077fe2
MohammadNasirAriaee <nasirariaee@gmail.com>
2026-08-09 13:50:12 +0430

    asdf

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -31,7 +31,7 @@ class CustomerController extends Controller
     public function create()
     {
         return Inertia::render('Customers/Create');
-        // add any additional data y
+        // add any additional data you need
     }
     public function store(Request $request)
     {
