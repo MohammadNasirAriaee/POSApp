68691ab1c647e423691f56fb9d4d9d3b3e73f00f
MohammadNasirAriaee <nasirariaee@gmail.com>
2026-08-11 15:40:44 +0430

    asdf

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -56,7 +56,7 @@ class CustomerController extends Controller
     {
         $data = $request->validate([
             'first_name' => 'required|string|max:255', // do not allow duplicate phone numbersasdf
-            'last_name' => 'nullable|string|max:255',
+            'last_name' => 'nullable|string|max:255', // do not allow duplicate phone numbers
             'email' => 'nullable|email|unique:customers,email,'.$customer->id, // do not allow duplicate emails
             'phone' => 'nullable|string|max:20|unique:customers,phone,'.$customer->id, // do not allow duplicate phone numbers
             'address' => 'nullable|string', // do not allow duplicate phone numbers
