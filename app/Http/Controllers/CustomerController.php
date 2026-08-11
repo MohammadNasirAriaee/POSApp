0a462f72d23314d89f8cc33f331d2c62816e3542
MohammadNasirAriaee <nasirariaee@gmail.com>
2026-08-11 15:41:08 +0430

    asdf

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -59,7 +59,7 @@ class CustomerController extends Controller
             'last_name' => 'nullable|string|max:255', // do not allow duplicate phone numbers
             'email' => 'nullable|email|unique:customers,email,'.$customer->id, // do not allow duplicate emails
             'phone' => 'nullable|string|max:20|unique:customers,phone,'.$customer->id, // do not allow duplicate phone numbers
-            'address' => 'nullable|string', /
+            'address' => 'nullable|string',
         ]);
 
         $customer->update($data);
