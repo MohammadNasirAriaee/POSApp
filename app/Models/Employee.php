581922807364941ea93c33fa1db9880c5f7b4d93
MohammadNasirAriaee <nasirariaee@gmail.com>
2026-08-12 10:45:49 +0430

    asdf

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -25,7 +25,7 @@ class Employee extends Model
         'Customer Support', // customer support position
     ];
 
-    protected $fillable = [ // for m
+    protected $fillable = [ // for mass assignment of employee attributes
         'first_name', // first name of the employee
         'last_name', // last name of the employee asdf
         'email',
