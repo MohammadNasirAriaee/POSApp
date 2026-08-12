4d4988a82d4ac28e055b99a822df11ab99cb530d
MohammadNasirAriaee <nasirariaee@gmail.com>
2026-08-12 14:14:24 +0430

    asdf

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -29,7 +29,7 @@ class Employee extends Model
         'first_name', // first  of the employee
         'last_name', // last name of the employee asdf
         'email',
-        'phone',
+        'phone', // phone number of the employee
         'address',
         'position',
         'salary',
