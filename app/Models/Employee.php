2d92749b1e6a1c61d4765bdc3e50f738b39532b3
MohammadNasirAriaee <nasirariaee@gmail.com>
2026-08-16 14:01:56 +0430

    asdf

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -12,7 +12,7 @@ class Employee extends Model
     use HasFactory;
 
     public const STATUS_ACTIVE = 'active'; // status for active employees
-    public const STATUS_INACTIVE = 'inactive'; // status for inactive employees
+    public const STATUS_INACTIVE = 'inactive'; // status for employees
     public const STATUS_ON_LEAVE = 'on_leave'; // status for on leave
 
     public const POSITIONS = [
