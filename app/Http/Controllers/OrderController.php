99347308f7cf621d2ad594cf8b68e61b4b142056
MohammadNasirAriaee <nasirariaee@gmail.com>
2026-08-20 10:10:17 +0430

    asdf

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -25,7 +25,7 @@ class OrderController extends Controller
     public function show(Order $order) // function to show order details
     {
         $order->load(['customer', 'employee', 'items.product']); // load related customer, employee, and product data for the order
-        return Inertia::render('Orders/Show', compact('order'));
+        return Inertia::render('Orders/Show', compact('order')); // return the show view with the order data
     }
 
     public function destroy(Order $order)
