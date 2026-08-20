b6f2e8059483b05cdf5f59eae333b2d8b882bfbf
MohammadNasirAriaee <nasirariaee@gmail.com>
2026-08-20 10:12:30 +0430

    asdf

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -16,7 +16,7 @@ class ProductController extends Controller
         $query = Product::with('category')->latest(); // get the latest products with their categories
 
         if ($search) { // if there is a search query, filter the products by name or SKU
-            $query->where(function ($q) use ($search) {
+            $query->where(function ($q) use ($search) { // define a closure to filter the products
                 $q->where('name', 'like', "%{$search}%")
                   ->orWhere('sku', 'like', "%{$search}%");
             });
