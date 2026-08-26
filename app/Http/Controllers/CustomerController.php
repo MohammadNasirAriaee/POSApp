537fa8e8e14d31ad7a3c820ad764d9d26820cd5c
MohammadNasirAriaee <nasirariaee@gmail.com>
2026-08-26 09:33:31 +0430

    asdf

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -55,7 +55,7 @@ class CustomerController extends Controller
     public function update(Request $request, Customer $customer) // function to update customer
     {
         $data = $request->validate([ // what to validate
-            'first_name' => 'required|string|max:255', // in it // bidfiber
+            'first_name' => 'required|string|max:255', // in it 
             'last_name' => 'nullable|string|max:255', // fiber // bit not
             'email' => 'nullable|email|unique:customers,email,' . $customer->id, // email must be unique except for the current customer
             'phone' => 'nullable|string|max:20|unique:customers,phone,' . $customer->id, // phone must be unique except for the current customer
