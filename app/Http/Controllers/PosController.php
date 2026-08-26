bf9afb5e96bcccd7acd16f663a3c94ba0dea7c6f
MohammadNasirAriaee <nasirariaee@gmail.com>
2026-08-26 09:38:45 +0430

    asdf

--- app/Http/Controllers/PosController.php
+++ app/Http/Controllers/PosController.php
@@ -19,7 +19,7 @@ class PosController extends Controller // controller for handling POS operations
         $query = Product::where('status', 'active');
 
         if ($request->filled('category_id')) { // if a category filter is applied, filter products by that category
-            $query->where('category_id', $request->category_id);
+            $query->where('category_id', $request->category_id); // added a filter for category_id to the product query
         }
 
         if ($request->filled('search')) {
