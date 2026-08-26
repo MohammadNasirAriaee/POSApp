c509fe1bf6bd50450f4040c43705c528dfd760f1
MohammadNasirAriaee <nasirariaee@gmail.com>
2026-08-26 09:33:54 +0430

    asdf

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -57,7 +57,7 @@ class CustomerController extends Controller
         $data = $request->validate([ // what to validate
             'first_name' => 'required|string|max:255', // in it
             'last_name' => 'nullable|string|max:255', // fiber // bit not
-            'email' => 'nullable|email|unique:customers,email,' . $customer->id, // email must  except for the current customer
+            'email' => 'nullable|email|unique:customers,email,' . $customer->id, // email must  except for the cuomer
             'phone' => 'nullable|string|max:20|unique:customers,phone,' . $customer->id, // phone must  for the current customer
             'address' => 'nullable|string', // got it well
         ]);
