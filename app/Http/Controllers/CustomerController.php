1a9b9c6194d724a8f5aed4a1a00cfbd7da917f4b
MohammadNasirAriaee <nasirariaee@gmail.com>
2026-08-26 09:36:46 +0430

    asdf

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -59,7 +59,7 @@ class CustomerController extends Controller
             'last_name' => 'nullable|string|max:255',
             'email' => 'nullable|email|unique:customers,email,' . $customer->id,
             'phone' => 'nullable|string|max:20|unique:customers,phone,' . $customer->id,
-            'address' => 'nullable|string', // got it well
+            'address' => 'nullable|string',
         ]);
 
         $customer->update($data);
