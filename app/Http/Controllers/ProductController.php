c5ef8f7c128153c9a524f93f5a46a88ec6bdb912
MohammadNasirAriaee <nasirariaee@gmail.com>
2026-08-30 13:14:07 +0430

    asdf

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -18,7 +18,7 @@ class ProductController extends Controller
         if ($search) {
             $query->where(function ($q) use ($search) { // define a closure to filter the products
                 $q->where('name', 'like', "%{$search}%") // search by name
-                  ->orWhere('sku', 'like', "%{$search}%"); // asdfghjklasd
+                  ->orWhere('sku', 'like', "%{$search}%"); // asdfghjklasdf
             }); // one agent
         }
 
