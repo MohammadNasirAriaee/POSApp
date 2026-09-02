b82228b7ade014beb7bbd43472a61701096722d5
MohammadNasirAriaee <nasirariaee@gmail.com>
2026-09-02 13:58:14 +0430

    asdf

--- app/Http/Requests/UpdateCustomerRequest.php
+++ app/Http/Requests/UpdateCustomerRequest.php
@@ -2,28 +2,24 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateCustomerRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['nullable', 'string', 'max:255'],
+            'email' => ['nullable', 'email', 'max:255', Rule::unique('customers', 'email')->ignore($this->route('customer'))],
+            'phone' => ['nullable', 'string', 'max:20', Rule::unique('customers', 'phone')->ignore($this->route('customer'))],
+            'address' => ['nullable', 'string'],
         ];
     }
 }
