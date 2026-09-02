1ee9b50d147811da394f7a6ff540b660c3a817bd
MohammadNasirAriaee <nasirariaee@gmail.com>
2026-09-02 14:09:39 +0430

    asdf

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Builder;
 
 class Category extends Model
 {
@@ -23,5 +24,10 @@ class Category extends Model
     public function products(): HasMany
     {
         return $this->hasMany(Product::class); // a category can have many products
+    }
+
+    public function scopeActive(Builder $query): Builder
+    {
+        return $query->where('is_active', true);
     }
 }
