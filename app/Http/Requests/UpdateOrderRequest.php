5966194fe8f5b93f52aee32773dc0ecd6c0e87e2
MohammadNasirAriaee <nasirariaee@gmail.com>
2026-09-03 09:59:24 +0430

    asfd

--- app/Http/Requests/UpdateOrderRequest.php
+++ app/Http/Requests/UpdateOrderRequest.php
@@ -2,28 +2,21 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateOrderRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'status' => ['sometimes', 'in:pending,completed,cancelled'],
+            'notes' => ['nullable', 'string', 'max:2000'],
+            'payment_method' => ['sometimes', 'in:cash,card,bank_transfer'],
         ];
     }
 }
