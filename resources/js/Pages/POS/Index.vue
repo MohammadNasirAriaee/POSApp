889f8709db71b842bf7c75c2bd7ffedab740ef0a
MohammadNasirAriaee <nasirariaee@gmail.com>
2026-09-08 09:25:37 +0430

    asdf

--- resources/js/Pages/POS/Index.vue
+++ resources/js/Pages/POS/Index.vue
@@ -334,6 +334,7 @@ const formatMoney = (amount) => {
                                 >
                                 <button
                                     @click="updateQuantity(item, 1)"
+                                    :aria-label="`Increase quantity of ${item.name}`"
                                     class="w-6 h-6 flex items-center justify-center rounded bg-white text-surface-600 hover:text-primary-600 hover:shadow-sm transition-all shadow-[0_1px_2px_rgba(0,0,0,0.05)]"
                                 >
                                     <Plus class="w-3 h-3" />
