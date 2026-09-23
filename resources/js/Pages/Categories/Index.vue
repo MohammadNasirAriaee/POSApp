aca7fc55921344b2826249a1159ea2194a88751f
MohammadNasirAriaee <nasirariaee@gmail.com>
2026-09-23 13:14:30 +0430

    Add a tooltip to the category search clear button

--- resources/js/Pages/Categories/Index.vue
+++ resources/js/Pages/Categories/Index.vue
@@ -69,6 +69,7 @@ const deleteCategory = (category) => {
                         v-if="searchQuery"
                         @click="clearSearch"
                         aria-label="Clear category search"
+                        title="Clear search"
                         class="absolute inset-y-0 right-0 flex items-center pr-3 text-surface-400 hover:text-surface-600"
                     >
                         <X class="w-4 h-4" />
